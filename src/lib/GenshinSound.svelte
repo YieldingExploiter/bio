fb33696bbef5903ce0a60bd6abb22149d6d4a6bb
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-11-09 02:28:26 +0100

    refactor: :recycle: move genshin ost to album subdir

--- src/lib/GenshinSound.svelte
+++ src/lib/GenshinSound.svelte
@@ -3,20 +3,20 @@
   import baseRoute from '../BaseRoute';
 
   const sounds = [
-    '/music/GenshinOST/A Day in Mondstadt [muSibQfpHTE].mp3',
-    '/music/GenshinOST/A Storm, A Spire, and A Sanctum (Dvalin\'s Nest) [d1q9ahJCpLc].mp3',
-    '/music/GenshinOST/A Sweet Smile [Ql4kh7XFsps].mp3',
-    '/music/GenshinOST/A Tale of Two Dragons [SgvHmGCVZqs].mp3',
-    '/music/GenshinOST/Caelestinum Finale Termini [VqtScyk2C5A].mp3',
-    '/music/GenshinOST/Dawn Winery Theme [wdfgkJwfit8].mp3',
-    '/music/GenshinOST/Genshin Impact Main Theme [EEYMlUVKmyE].mp3',
-    '/music/GenshinOST/Happy Journey [zfdIFm6CDe8].mp3',
-    '/music/GenshinOST/Knighthood Excellence [uZC1F1pAAZM].mp3',
-    '/music/GenshinOST/Lone Sojourner [IaXxCLXbowg].mp3',
-    '/music/GenshinOST/Pure Sky [qHNvq6mBNg4].mp3',
-    '/music/GenshinOST/Rite of Battle [M3CoUlbjQMs].mp3',
-    '/music/GenshinOST/The Edge of the Prairie [udfm0ff2XOg].mp3',
-    '/music/GenshinOST/The Outlander Who Caught the Wind [Asa9D0HwJgA].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/A Day in Mondstadt [muSibQfpHTE].mp3',
+    "/music/GenshinOST/The Wind and The Star Traveler/A Storm, A Spire, and A Sanctum (Dvalin's Nest) [d1q9ahJCpLc].mp3",
+    '/music/GenshinOST/The Wind and The Star Traveler/A Sweet Smile [Ql4kh7XFsps].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/A Tale of Two Dragons [SgvHmGCVZqs].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/Caelestinum Finale Termini [VqtScyk2C5A].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/Dawn Winery Theme [wdfgkJwfit8].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/Genshin Impact Main Theme [EEYMlUVKmyE].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/Happy Journey [zfdIFm6CDe8].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/Knighthood Excellence [uZC1F1pAAZM].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/Lone Sojourner [IaXxCLXbowg].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/Pure Sky [qHNvq6mBNg4].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/Rite of Battle [M3CoUlbjQMs].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/The Edge of the Prairie [udfm0ff2XOg].mp3',
+    '/music/GenshinOST/The Wind and The Star Traveler/The Outlander Who Caught the Wind [Asa9D0HwJgA].mp3',
   ].map((v) => `${baseRoute}${v}`);
   const getRandomSound = () =>
     sounds[Math.floor(Math.random() * sounds.length)];
